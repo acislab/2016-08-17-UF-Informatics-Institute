5bf4381865b1980c1c30900be22dea8b5b926b14
Matthew Collins <mcollins@acis.ufl.edu>
2016-08-17 22:06:27 -0400

    Small script showing working with databases from R

---
+++ graphs.R
@@ -0,0 +1,51 @@
+# This script require packages, install them once
+# install.packages("RSQLite")
+# install.packages("ggplot2")
+# install.packages("plyr")
+
+
+# Download database from source if not present
+sqlite_url <- ""
+sqlite_file <- "data/survey.sqlite"
+if (!file.exists(sqlite_file)){
+  download.file(url=sqlite_url,
+                destfile=sqlite_file)
+}
+
+# Connect to the database
+library(RSQLite)
+driver <- dbDriver("SQLite")
+con <- dbConnect(drv=driver,
+                 dbname=sqlite_file)
+
+# Use this to see the tables
+# dbListTables(con)
+
+# Select data in to a dataframe
+sql_statement <- "
+SELECT Survey.*, Visited.dated
+FROM Survey JOIN Visited
+ON Survey.taken = Visited.id"
+
+results <- dbSendQuery(con, sql_statement)
+readings <- fetch(results)
+
+# Add a year column
+readings <- cbind(readings,
+                  year=substr(readings$dated, 1, 4))
+
+# Select only salinities
+salinities <- readings[which(readings$quant == "sal"),]
+
+# Remove salinities over 1, they are outside the
+# valid range for the quantity and must be an
+# error
+salinities <- salinities[which(salinities$reading <= 1),]
+
+# Make graph
+library(ggplot2)
+p <- qplot(year, reading, data=salinities)
+print(p)
+
+dbClearResult(results)
+dbDisconnect(con)
